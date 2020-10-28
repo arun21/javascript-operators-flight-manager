3d02fed79b6a5752b98cb1c2e0c20fed14d44ceb
arun.prabhu <arun.prabhu@mobilelive.ca>
2020-10-28 14:38:51 -0400

    Manage flights and passengers

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,17 @@
 function Passengers() {
+  function checkFlightCapacity(capacity, passengersArray) {
+    let total = 0;
+    total = passengersArray.reduce((acc, value) => {
+      return acc + value;
+    });
 
+    if (total < capacity) {
+      return total;
+    } else {
+      throw new Error("total passengers are more than the capacity");
+    }
+  }
+  return checkFlightCapacity();
 }
 
+module.exports = Passengers();
